335e7efae41aba3c1d974ed07d74b2f5d440c15f
Jon Esparaz <jon.esparaz@gmail.com>
2020-10-07 22:10:33 -0400

    connect copy

--- screens/MoreScreen.tsx
+++ screens/MoreScreen.tsx
@@ -84,7 +84,7 @@ function MoreScreen(): JSX.Element {
     const items = [
         { id: "give", text: "Give", subtext: "Donate to The Meeting House", icon: Theme.icons.white.give, action: () => Linking.openURL('https://www.themeetinghouse.com/give') },
         //{ id: "volunteer", text: "Volunteer", subtext: "Help out your local community", icon: Theme.icons.white.volunteer },
-        { id: "connect", text: "Connect", subtext: "Get connected with a staff member", icon: Theme.icons.white.connect, action: () => Linking.openURL('https://www.themeetinghouse.com/connect') },
+        { id: "connect", text: "Connect", subtext: "Looking to connect with us?", icon: Theme.icons.white.connect, action: () => Linking.openURL('https://www.themeetinghouse.com/connect') },
         //{ id: "staff", text: "Staff Directory", subtext: "Contact a staff member directly", icon: Theme.icons.white.staff },
         { id: "homeChurch", text: "Home Church", subtext: "Find a home church near you", icon: Theme.icons.white.homeChurch, action: () => Linking.openURL('https://www.themeetinghouse.com/find-homechurch') },
     ]
