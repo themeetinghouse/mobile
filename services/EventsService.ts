4ddd6c579786e2b038e03e985fe03617867ab081
Igor Belem <igortbelem@gmail.com>
2020-10-10 18:23:16 -0400

    wip sorting events by date

--- services/EventsService.ts
+++ services/EventsService.ts
@@ -7,15 +7,15 @@ export type EventQueryResult = NonNullable<GetFbEventsQuery['getFBEvents']>['dat
 
 export default class EventsService {
 
-  static loadEventsList = async (location: Location | null | undefined): Promise<EventQueryResult> => {
-    console.log(`logging location ${JSON.stringify(location)}`)
+  static loadEventsList = async (location: Location | null): Promise<EventQueryResult> => {
+    //console.log(`logging location ${JSON.stringify(location)}`)
 
     const locations = await LocationService.loadLocations();
-    const currentLocation = locations.filter((ayy) => {
-      return ayy.id === location?.locationId;
+    const currentLocation = locations.filter((loc) => {
+      return loc.id === location?.locationId;
     })
     let x: any;
-    await fetch(`https://www.themeetinghouse.com/static/content/${currentLocation[0].id}.json`)
+    await fetch(`https://www.themeetinghouse.com/static/content/${currentLocation[0].id}.json`) //put this in a variable and await its result to return
       .then((response) => response.json())
       .then((data) => {
         const item: any = data?.page?.content.filter((item: any) => {
@@ -24,14 +24,19 @@ export default class EventsService {
         x = item[0]?.facebookEvents;
         return x;
       })
-    console.log("x is " + x)
     if (x !== null) {
       const query = {
         query: getFbEvents,
         variables: { pageId: x[0] },
       }
       const queryResult = await runGraphQLQuery(query);
-      return queryResult.getFBEvents.data;      // sorting needs to occur here
+      /*
+      console.log(queryResult.getFBEvents.data.sort((a: any, b: any) => {
+        console.log(a.start_time < b.start_time)
+        return a.start_time < b.start_time
+      }))
+      */
+      return queryResult.getFBEvents.data;    // sorting needs to occur here
     }
   }
 }
