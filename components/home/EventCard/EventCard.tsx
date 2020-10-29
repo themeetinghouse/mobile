99c8802491ca42307ded5f7276cec9967465875e
Igor Belem <igortbelem@gmail.com>
2020-10-29 11:30:28 -0400

    improvements to EventCard full address should now be omitted removed whitespace from description

--- components/home/EventCard/EventCard.tsx
+++ components/home/EventCard/EventCard.tsx
@@ -56,7 +56,16 @@ type EventCardInput = {
 }
 
 export default function EventCard({ event, handlePress }: EventCardInput): JSX.Element {
+    const addressFilter = (address: string) => {
+        console.log("returning :" + address.split(',')[0])
+        return address.split(',')[0]
+    }
+    const descriptionFilter = (description: string) => {
+        return description.replace(/(\r?\n)\s*\1+/g, '$1');
+    }
+
     const dateStr = moment(event?.start_time ?? undefined).format('MMM D');
+    console.log(event)
     return (
 
         <TouchableHighlight underlayColor={Theme.colors.grey5} onPress={handlePress}>
@@ -66,11 +75,11 @@ export default function EventCard({ event, handlePress }: EventCardInput): JSX.E
                     <Text uppercase={false} style={style.title}>{event?.name}</Text>
                     <Thumbnail style={style.icon} source={Theme.icons.white.arrow}></Thumbnail>
                 </View>
-                <Text style={style.descriptionContainer} numberOfLines={4}>{event?.description}</Text>
+                <Text style={style.descriptionContainer} numberOfLines={4}>{event?.description ? descriptionFilter(event?.description) : null}</Text>
                 {event?.place?.name || event?.place?.location?.street ?
-                    <Text style={style.locationContainer}>{event?.place?.name ? event.place.name + "," : null} {event?.place?.location?.street ? event.place.location.street : null}</Text>
+                    <Text style={style.locationContainer}>{event?.place?.name ? addressFilter(event.place.name) : null} {event?.place?.location?.street ? "," + addressFilter(event.place.location.street) : null} </Text>
                     : null}
-                <Text style={style.dateTimeContainer}>{moment(event?.start_time).format("h:mm a")}  {event?.end_time ? "- " + moment(event?.end_time).format("h:mm a") : ""}</Text>
+                <Text style={style.dateTimeContainer}>{moment(event?.start_time).format("h:mm")}  {event?.end_time ? "- " + moment(event?.end_time).format("h:mm a") : ""}</Text>
             </View>
         </TouchableHighlight>
 
