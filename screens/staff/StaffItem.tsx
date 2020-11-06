92aa29235dd75bc3ab3322fdb4ef3de32c809186
Igor Belem <igortbelem@gmail.com>
2020-11-06 00:10:42 -0500

    added placeholder image

--- screens/staff/StaffItem.tsx
+++ screens/staff/StaffItem.tsx
@@ -2,7 +2,7 @@ import { Thumbnail } from 'native-base';
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Theme, Style } from '../../Theme.style';
-import { Image } from "react-native";
+import { TouchableHighlight, TouchableOpacity } from "react-native-gesture-handler"
 const style = StyleSheet.create({
     container: {
         marginTop: 6,
@@ -14,7 +14,7 @@ const style = StyleSheet.create({
     },
     picture: {
         marginTop: 0,
-        backgroundColor: "white",
+        backgroundColor: "#54565A",
         borderRadius: 100,
         width: 48,
         height: 48
@@ -43,33 +43,46 @@ const style = StyleSheet.create({
     icon: { ...Style.icon, width: 21, height: 21, },
     iconContainer: {
         justifyContent: "center",
-        margin: 2,
-        padding: 0
+        padding: 16
     },
 })
 interface Staff {
     staff: {
         name: string
         title: string
+        teaching: string
+        phone: string
+        email: string
     }
 }
 export default function StaffItem({ staff }: Staff): JSX.Element {
     return (
         <View style={style.container}>
-            <View style={style.picture}></View>
+            <Thumbnail style={[style.picture]} source={Theme.icons.white.user}></Thumbnail>
             <View style={{ marginLeft: 15, flexDirection: "column" }}>
-                <Text style={style.name}>{staff.name}</Text>
-                <Text style={style.title}>{staff.title}</Text>
-                <Text style={style.footerText}>View Teaching</Text>
+                {staff.name ?
+                    <Text style={style.name}>{staff.name}</Text>
+                    : null}
+                {staff.title ?
+                    <Text style={style.title}>{staff.title}</Text>
+                    : null}
+                {staff.teaching ?
+                    <TouchableOpacity>
+                        <Text style={style.footerText}>
+                            View Teaching
+                    </Text>
+                    </TouchableOpacity>
+                    : null}
+
             </View>
-            <View style={{ flexDirection: "column", flex: 1, margin: 10 }}>
-                <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
-                    <View style={style.iconContainer}>
+            <View style={{ flexDirection: "column", flex: 1, }}>
+                <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
+                    <TouchableOpacity onPress={() => console.log("press")} style={style.iconContainer}>
                         <Thumbnail style={style.icon} source={Theme.icons.white.phone} square></Thumbnail>
-                    </View>
-                    <View style={style.iconContainer}>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => console.log("press")} style={style.iconContainer}>
                         <Thumbnail style={style.icon} source={Theme.icons.white.contact} square></Thumbnail>
-                    </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
