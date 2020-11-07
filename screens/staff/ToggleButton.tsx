2ff8edb3bb1ebbbd8ac7ffa1aefe3260c5f396cd
Igor Belem <igortbelem@gmail.com>
2020-11-07 12:05:54 -0500

    fixes to togglebutton component ui

--- screens/staff/ToggleButton.tsx
+++ screens/staff/ToggleButton.tsx
@@ -7,7 +7,8 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const style = StyleSheet.create({
     button: {
-        flex: 1,
+        minWidth: "50%",
+        flexGrow: 1,
         padding: 16,
         margin: 2,
         borderRadius: 50,
@@ -15,19 +16,22 @@ const style = StyleSheet.create({
         backgroundColor: "#1A1A1A",
     },
     selectedButton: {
+        minWidth: "50%",
         padding: 16,
-        flex: 1,
+        flexGrow: 1,
         margin: 2,
         borderRadius: 50,
         color: "#FFF",
         backgroundColor: "#646469",
     },
     buttonText: {
-        color: "white",
-        width: 140,
+        color: "#C8C8C8",
         textAlign: "center",
         fontFamily: Theme.fonts.fontFamilyBold,
         fontSize: Theme.fonts.small
+    },
+    selectedButtonText: {
+        color: "white",
     },
     buttonContainer: {
         height: 38,
@@ -53,10 +57,10 @@ export default function ToggleButton(props: Props): JSX.Element {
 
         <View style={style.buttonContainer}>
             <TouchableOpacity onPress={() => setSortByName(false)} style={!sortByName ? style.selectedButton : style.button}>
-                <View style={{ justifyContent: "center", flex: 1 }}><Text style={style.buttonText}>{btnText_one}</Text></View>
+                <View style={{ justifyContent: "center", flex: 1 }}><Text style={!sortByName ? [style.buttonText, style.selectedButtonText] : style.buttonText}>{btnText_one}</Text></View>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => setSortByName(true)} style={sortByName ? style.selectedButton : style.button}>
-                <View style={{ justifyContent: "center", flex: 1 }}><Text style={style.buttonText}>{btnText_two}</Text></View>
+                <View style={{ justifyContent: "center", flex: 1 }}><Text style={sortByName ? [style.buttonText, style.selectedButtonText] : style.buttonText}>{btnText_two}</Text></View>
             </TouchableOpacity>
         </View>
     )
