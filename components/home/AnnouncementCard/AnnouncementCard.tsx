04af8dee193c3ac0e72adf66e4434b3a5d4ec99c
Igor Belem <igortbelem@gmail.com>
2020-12-31 08:50:17 -0500

    added pictures to announcementcard.tsx

--- components/home/AnnouncementCard/AnnouncementCard.tsx
+++ components/home/AnnouncementCard/AnnouncementCard.tsx
@@ -4,10 +4,13 @@ import Theme, { Style } from '../../../Theme.style';
 //import WhiteButton from '../../buttons/WhiteButton';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Announcement } from '../../../services/AnnouncementService';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const style = StyleSheet.create({
     cardContainer: {
+        height: 300,
+        flexWrap: "nowrap",
         paddingLeft: 16,
         paddingRight: 16,
         paddingBottom: 40,
@@ -16,6 +19,14 @@ const style = StyleSheet.create({
         marginBottom: 16,
         borderBottomWidth: 1,
         borderBottomColor: "#313131",
+    },
+    imageCardContainer: {
+        height: 300,
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingBottom: 40,
+        paddingTop: 40,
+        marginBottom: 16,
     },
     title: { ...Style.title, ...{ marginBottom: 8 } },
     body: Style.body,
@@ -36,10 +47,31 @@ type AnnouncementCardInput = {
 
 export default function AnnouncementCard({ announcement, handlePress }: AnnouncementCardInput): JSX.Element {
     return (
-        <TouchableWithoutFeedback style={style.cardContainer} onPress={handlePress}>
-            <Thumbnail style={style.icon} source={Theme.icons.white.announcement}></Thumbnail>
-            <Text style={style.title}>{announcement.title}</Text>
-            <Text style={style.body}>{announcement.description.slice(0, 200).trim() + "..."}</Text>
-        </TouchableWithoutFeedback>
+        announcement?.image ?
+            <View style={{ paddingBottom: 16, backgroundColor: Theme.colors.black, }}>
+                <ImageBackground style={{ flex: 1, backgroundColor: Theme.colors.gray1, borderBottomWidth: 1, borderBottomColor: "#313131", }} source={{ uri: announcement.image }}>
+                    <LinearGradient
+                        colors={["rgba(255,255,255, 0)", "rgba(0,0,0, 0.9)"]}
+                        style={{
+                            position: 'absolute',
+                            left: 0,
+                            right: 0,
+                            top: 0,
+                            height: "100%"
+                        }}
+                    />
+                    <TouchableWithoutFeedback style={style.imageCardContainer} onPress={handlePress}>
+                        <Thumbnail style={style.icon} source={Theme.icons.white.announcement}></Thumbnail>
+                        <Text style={style.title}>{announcement.title}</Text>
+                        <Text style={style.body}>{announcement.description.slice(0, 100).trim() + "..."}</Text>
+                    </TouchableWithoutFeedback>
+                </ImageBackground>
+            </View>
+            :
+            <TouchableWithoutFeedback style={style.cardContainer} onPress={handlePress}>
+                <Thumbnail style={style.icon} source={Theme.icons.white.announcement}></Thumbnail>
+                <Text style={style.title}>{announcement.title}</Text>
+                <Text style={style.body}>{announcement.description.slice(0, 100).trim() + "..."}</Text>
+            </TouchableWithoutFeedback>
     )
 }
