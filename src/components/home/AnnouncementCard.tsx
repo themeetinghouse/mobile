d45368174420aef550dcacae542a1d2aa702328c
Igor Belem <igortbelem@gmail.com>
2021-01-06 19:14:41 -0500

    added announcementDetailscreen and card changes,

--- src/components/home/AnnouncementCard.tsx
+++ src/components/home/AnnouncementCard.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, Thumbnail } from 'native-base';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import Theme, { Style } from '../../Theme.style';
 import { Announcement } from '../../services/AnnouncementService';
 
 const style = StyleSheet.create({
   cardContainer: {
+    height: 300,
+    flexWrap: 'nowrap',
     paddingLeft: 16,
     paddingRight: 16,
     paddingBottom: 40,
@@ -15,6 +18,14 @@ const style = StyleSheet.create({
     marginBottom: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#313131',
+  },
+  imageCardContainer: {
+    height: 300,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 40,
+    paddingTop: 40,
+    marginBottom: 16,
   },
   title: { ...Style.title, ...{ marginBottom: 8 } },
   body: Style.body,
@@ -28,20 +39,59 @@ const style = StyleSheet.create({
   },
 });
 
-interface Props {
+type AnnouncementCardInput = {
   announcement: Announcement;
   handlePress(): void;
-}
+};
 
 export default function AnnouncementCard({
   announcement,
   handlePress,
-}: Props): JSX.Element {
-  return (
+}: AnnouncementCardInput): JSX.Element {
+  return announcement?.image ? (
+    <View style={{ paddingBottom: 16, backgroundColor: Theme.colors.black }}>
+      <ImageBackground
+        style={{
+          flex: 1,
+          backgroundColor: Theme.colors.gray1,
+          borderBottomWidth: 1,
+          borderBottomColor: '#313131',
+        }}
+        source={{ uri: announcement.image ?? '' }}
+      >
+        <LinearGradient
+          colors={['rgba(0,0,0, 0.2)', 'rgba(0,0,0, 0.9)']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: '100%',
+          }}
+        />
+        <TouchableWithoutFeedback
+          style={style.imageCardContainer}
+          onPress={handlePress}
+        >
+          <Thumbnail
+            style={style.icon}
+            source={Theme.icons.white.announcement}
+          />
+          <Text style={style.title}>{announcement.title}</Text>
+          <Text style={style.body}>
+            {`${announcement.description.slice(0, 100).trim()}...`}
+          </Text>
+        </TouchableWithoutFeedback>
+      </ImageBackground>
+    </View>
+  ) : (
     <TouchableWithoutFeedback style={style.cardContainer} onPress={handlePress}>
       <Thumbnail style={style.icon} source={Theme.icons.white.announcement} />
-      <Text style={style.title}>{announcement.title}</Text>
-      <Text style={style.body}>{announcement.description}</Text>
+      <Text style={style.title}>{announcement?.title}</Text>
+      <Text style={style.body}>
+        {announcement?.description.slice(0, 100).trim()}{' '}
+        {announcement?.description?.length > 100 ? '...' : ''}
+      </Text>
     </TouchableWithoutFeedback>
   );
 }
--- src/screens/home/AnnouncementDetailsScreen.tsx
+++ src/screens/home/AnnouncementDetailsScreen.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Container, Text, Button, Icon, Content, View } from 'native-base';
-import { StyleSheet } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { Text, Thumbnail, Content, View } from 'native-base';
+import { Dimensions, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+import { LinearGradient } from 'expo-linear-gradient';
 import { HomeStackParamList } from '../../navigation/MainTabNavigator';
 import WhiteButton from '../../components/buttons/WhiteButton';
 import { Theme, Style, HeaderStyle } from '../../Theme.style';
+import { Announcement } from '../../services/AnnouncementService';
 
 const style = StyleSheet.create({
   content: {
     ...Style.cardContainer,
     ...{
-      backgroundColor: Theme.colors.black,
       padding: 16,
     },
   },
@@ -39,7 +41,7 @@ const style = StyleSheet.create({
   title: {
     ...Style.title,
     ...{
-      marginTop: 130,
+      marginTop: 140,
       marginBottom: 16,
     },
   },
@@ -60,33 +62,85 @@ export default function AnnouncementDetailScreen({
   navigation,
   route,
 }: Props): JSX.Element {
-  const announcementItem = route.params?.item;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: 'Announcement',
+      headerTitleStyle: style.headerTitle,
+      headerStyle: { backgroundColor: Theme.colors.background },
+      headerLeft: function render() {
+        return (
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Thumbnail
+              square
+              source={Theme.icons.white.closeCancel}
+              style={{ width: 24, height: 24 }}
+            />
+          </TouchableOpacity>
+        );
+      },
+      headerLeftContainerStyle: { left: 16 },
+      headerRight: function render() {
+        return <View style={{ flex: 1 }} />;
+      },
+    });
+  }, [navigation]);
 
-  navigation.setOptions({
-    headerShown: true,
-    title: 'Announcement',
-    headerTitleStyle: style.headerTitle,
-    headerStyle: { backgroundColor: Theme.colors.background },
-    headerLeft: function render() {
-      return (
-        <Button transparent onPress={() => navigation.goBack()}>
-          <Icon name="close" />
-        </Button>
-      );
-    },
-    headerLeftContainerStyle: { left: 16 },
-    headerRight: function render() {
-      return <View style={{ flex: 1 }} />;
-    },
-  });
-
+  const announcementItem: Announcement = route.params?.item;
+  const parseUrl = () => {
+    if (announcementItem.callToAction) {
+      if (announcementItem.callToAction.includes('https://')) {
+        return announcementItem.callToAction;
+      }
+      return `https://${announcementItem.callToAction}`;
+    }
+    return '';
+  };
   return (
-    <Container>
+    <>
+      {announcementItem.image ? (
+        <>
+          <Image
+            style={{
+              top: 0,
+              position: 'absolute',
+              height: 200,
+              width: Dimensions.get('window').width,
+            }}
+            source={{ uri: announcementItem.image }}
+          />
+          <LinearGradient
+            colors={['rgba(0,0,0, 0.05)', 'rgba(0,0,0, 0.9)']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              height: 200,
+            }}
+          />
+        </>
+      ) : null}
       <Content style={style.content}>
         <Text style={style.title}>{announcementItem.title}</Text>
         <Text style={style.body}>{announcementItem.description}</Text>
-        <WhiteButton label="Call to action" />
+        {announcementItem.callToAction ? (
+          <WhiteButton
+            style={{ height: 56, marginBottom: 20, marginTop: 20 }}
+            label="Call to Action"
+            onPress={() => {
+              Linking.openURL(parseUrl() ?? '');
+            }}
+          />
+        ) : null}
       </Content>
-    </Container>
+    </>
   );
 }
