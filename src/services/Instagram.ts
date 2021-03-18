a73c3faf54955c2c541fbd51d252122f2c1df784
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-03-18 01:30:01 -0400

    patch: filter videos from instagram data (#242)

--- src/services/Instagram.ts
+++ src/services/Instagram.ts
@@ -87,23 +87,30 @@ export default class InstagramService {
       const json = (await API.graphql(
         graphqlOperation(getInstaPhotos, query)
       )) as GraphQLResult<GetInstaPhotosQuery>;
-      const images = json.data?.getInstaPhotos?.data;
-
-      if (images && images.length > 0) {
-        return { images, username };
-      }
-
-      if (pageId !== '17841400321603203') {
-        const fallbackQuery: GetInstaPhotosQueryVariables = {
-          pageId: '17841400321603203',
-        };
-        const jsonFallback = (await API.graphql(
-          graphqlOperation(getInstaPhotos, fallbackQuery)
-        )) as GraphQLResult<GetInstaPhotosQuery>;
-        const backupImages = jsonFallback.data?.getInstaPhotos?.data;
-        if (backupImages) {
-          return { images: backupImages, username: 'themeetinghouse' };
+      if (!json.data?.getInstaPhotos?.data?.length) {
+        if (pageId !== '17841400321603203') {
+          const fallbackQuery: GetInstaPhotosQueryVariables = {
+            pageId: '17841400321603203',
+          };
+          const jsonFallback = (await API.graphql(
+            graphqlOperation(getInstaPhotos, fallbackQuery)
+          )) as GraphQLResult<GetInstaPhotosQuery>;
+          const oakvilleImages = jsonFallback.data?.getInstaPhotos?.data;
+          if (oakvilleImages) {
+            return { images: oakvilleImages, username: 'themeetinghouse' };
+          }
         }
+      } else {
+        const { data } = json.data.getInstaPhotos;
+        const { length } = data;
+        const images = [];
+        let i = 0;
+
+        while (i < length && images.length < 8) {
+          if (data[i]?.media_type !== 'VIDEO') images.push(data[i]);
+          i += 1;
+        }
+        return { images, username };
       }
     } catch (e) {
       console.debug(e);
