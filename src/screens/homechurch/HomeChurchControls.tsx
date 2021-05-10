d4ddf2e3bd9c1365a9c3986ddee8361584926eb6
Igor Belem <igortbelem@gmail.com>
2021-05-10 10:34:24 -0400

    Disabled "Clear All" when dropdown is open

--- src/screens/homechurch/HomeChurchControls.tsx
+++ src/screens/homechurch/HomeChurchControls.tsx
@@ -9,19 +9,25 @@ import {
 } from 'react-native';
 import { Thumbnail } from 'native-base';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 import { MainStackParamList } from '../../navigation/AppNavigator';
 import { LocationData } from '../../contexts/LocationContext';
 import { Theme, Style } from '../../Theme.style';
-import { TouchableHighlight } from 'react-native-gesture-handler';
 
 interface Params {
   navigation: StackNavigationProp<MainStackParamList>;
   loc: LocationData;
   setDay: (day: string) => void;
 }
-const HomeChurchControls = ({ loc, navigation, setDay }: Params): JSX.Element => {
+const HomeChurchControls = ({
+  loc,
+  navigation,
+  setDay,
+}: Params): JSX.Element => {
   const [selectedLocation, setSelectedLocation] = useState<LocationData>(
-    loc?.locationName === 'unknown' || !!!loc?.locationName ? { locationName: 'All Locations', locationId: "" } : { locationName: loc?.locationName, locationId: loc?.locationId }
+    loc?.locationName === 'unknown' || !loc?.locationName
+      ? { locationName: 'All Locations', locationId: '' }
+      : { locationName: loc?.locationName, locationId: loc?.locationId }
   );
   const [weekday, setWeekDay] = useState('All Days');
   const days = [
@@ -41,7 +47,7 @@ const HomeChurchControls = ({ loc, navigation, setDay }: Params): JSX.Element =>
       backgroundColor: '#111111',
       margin: 16,
       width: Dimensions.get('window').width - 32,
-      position: 'relative'
+      position: 'relative',
     },
     containerItem: {
       flexDirection: 'row',
@@ -64,18 +70,16 @@ const HomeChurchControls = ({ loc, navigation, setDay }: Params): JSX.Element =>
   const handleDrop = (day: string) => {
     setWeekDay(day);
     setDay(day);
-    setActive(false)
-  }
+    setActive(false);
+  };
   useEffect(() => {
-    setSelectedLocation(loc)
-  }, [loc])
+    setSelectedLocation(loc);
+  }, [loc]);
   return (
     <>
       <View style={style.container}>
         <TouchableWithoutFeedback
-          onPress={() =>
-            navigation.push('HomeChurchLocationSelect', {})
-          }
+          onPress={() => navigation.push('HomeChurchLocationSelect', {})}
           style={style.containerItem}
         >
           <View
@@ -90,10 +94,16 @@ const HomeChurchControls = ({ loc, navigation, setDay }: Params): JSX.Element =>
               source={Theme.icons.white.location}
               square
             />
-            <Text style={style.locationSelect}>{selectedLocation?.locationName}</Text>
+            <Text style={style.locationSelect}>
+              {selectedLocation?.locationName}
+            </Text>
           </View>
         </TouchableWithoutFeedback>
-        <TouchableHighlight onBlur={() => setActive(false)} style={style.containerItem} onPress={() => setActive(!active)}>
+        <TouchableHighlight
+          onBlur={() => setActive(false)}
+          style={style.containerItem}
+          onPress={() => setActive(!active)}
+        >
           <Text style={{ color: 'white', padding: 16 }}>{weekday}</Text>
         </TouchableHighlight>
 
@@ -102,43 +112,47 @@ const HomeChurchControls = ({ loc, navigation, setDay }: Params): JSX.Element =>
             style={{
               backgroundColor: '#111111',
               top: 56,
-              width: "100%",
+              width: '100%',
               position: 'absolute',
               zIndex: 1000,
             }}
           >
             {days.map((day) => (
-              <TouchableHighlight key={day} onPress={() => handleDrop(day)} style={style.containerItem}>
+              <TouchableHighlight
+                key={day}
+                onPress={() => handleDrop(day)}
+                style={style.containerItem}
+              >
                 <Text style={{ color: 'white', padding: 16 }}>{day}</Text>
               </TouchableHighlight>
             ))}
           </View>
         ) : null}
-
       </View>
-      <View style={{ zIndex: -100 }}>
-
-        <TouchableOpacity
-          style={{ zIndex: -100, backgroundColor: "grey", width: 100 }}
-          onPress={() => {
-            handleDrop('All Days');
-            setSelectedLocation({ locationName: 'All Locations', locationId: "all" });
+      <TouchableOpacity
+        disabled={active}
+        style={{ zIndex: -1 }}
+        onPress={() => {
+          handleDrop('All Days');
+          setSelectedLocation({
+            locationName: 'All Locations',
+            locationId: 'all',
+          });
+        }}
+      >
+        <Text
+          style={{
+            alignSelf: 'flex-start',
+            marginLeft: 16,
+            marginBottom: 8,
+            color: Theme.colors.gray5,
+            textDecorationLine: 'underline',
+            fontFamily: Theme.fonts.fontFamilyRegular,
           }}
         >
-          <Text
-            style={{
-              alignSelf: 'flex-start',
-              marginLeft: 16,
-              marginBottom: 8,
-              color: Theme.colors.gray5,
-              textDecorationLine: 'underline',
-              fontFamily: Theme.fonts.fontFamilyRegular,
-            }}
-          >
-            Clear All
+          Clear All
         </Text>
-        </TouchableOpacity>
-      </View>
+      </TouchableOpacity>
     </>
   );
 };
