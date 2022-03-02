97c3589bcd70bf0fe91eff5f1aeddd7767ab8438
Igor Belem <igortbelem@gmail.com>
2022-03-02 13:45:19 -0500

    Removed unused import

--- src/screens/more/MoreScreen.tsx
+++ src/screens/more/MoreScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import React, { useContext, useLayoutEffect, useState } from 'react';
 import {
   Image,
   ImageSourcePropType,
